402ae210010f950556a026d6a13599a3c15d1149
wonho <wonho1401@naver.com>
2023-05-09 23:07:28 +0900

    style: 폰트, 이미지 추가

--- Frontend/mcthemax/src/App.js
+++ Frontend/mcthemax/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
