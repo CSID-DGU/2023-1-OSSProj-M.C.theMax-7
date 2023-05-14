e5fcb425b2da8a02b18070e93f9d0e3877beb876
pgrgrgrgr <parkracoon@naver.com>
2023-05-14 17:06:40 +0900

    fix: test ìubuntu상의 nginxê 구동을 위한 test class 주ì 처리

--- Backend/mcthemax/src/test/java/com/mcthemax/ApplicationTests.java
+++ Backend/mcthemax/src/test/java/com/mcthemax/ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mcthemax;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.mcthemax;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
