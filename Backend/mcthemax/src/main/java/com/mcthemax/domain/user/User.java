28276436c8416950dc6190df6fbe8f9515f14563
pgrgrgrgr <parkracoon@naver.com>
2023-06-01 02:13:19 +0900

    Feat: User 엔티티 생성자 추가

--- Backend/mcthemax/src/main/java/com/mcthemax/domain/user/User.java
+++ Backend/mcthemax/src/main/java/com/mcthemax/domain/user/User.java
@@ -1,13 +1,24 @@
 package com.mcthemax.domain.user;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name="user")
 @Getter @Setter
-public class User {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
 
     @Id
     @Column(name="id")
@@ -26,4 +37,38 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserStatus authority; //PROFESSOR, STUDENT
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(authority.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.pw;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
