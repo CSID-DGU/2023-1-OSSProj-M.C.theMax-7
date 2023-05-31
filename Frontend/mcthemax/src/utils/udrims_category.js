fdfc27f265540e7b06b2d74d0ed59e566eca406e
wonho <wonho1401@naver.com>
2023-06-01 01:55:38 +0900

    feat: mdrims 카테고리 설정

---
+++ Frontend/mcthemax/src/utils/udrims_category.js
@@ -0,0 +1,25 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: "원스탑",
+    subcategories: [{ subCategoryId: 1, name: "대표권한변경" }],
+  },
+  {
+    id: 2,
+    name: "학사정보",
+    subcategories: [
+      {
+        subCategoryId: 1,
+        name: "학적부 열람",
+      },
+      {
+        subCategoryId: 2,
+        name: "개인 강의시간표 조회",
+      },
+      {
+        subCategoryId: 3,
+        name: "금학기 성적관리",
+      },
+    ],
+  },
+];
