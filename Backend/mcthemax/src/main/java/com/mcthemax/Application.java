db1a2d98a6900e06100e7be977ae0b8fa465bbff
kwan2 <jgh9987@naver.com>
2023-06-01 19:34:54 +0900

    test: 루트 경로 hello world! 출력

--- Backend/mcthemax/src/main/java/com/mcthemax/Application.java
+++ Backend/mcthemax/src/main/java/com/mcthemax/Application.java
@@ -2,10 +2,17 @@ package com.mcthemax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
