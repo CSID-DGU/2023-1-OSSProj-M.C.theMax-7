1b1fed12bd4d9a4385047e3db7e83b8b4ceca12f
wonho <wonho1401@naver.com>
2023-06-02 01:07:15 +0900

    style: Global Style 추가

--- Frontend/mcthemax/src/assets/color/color.js
+++ Frontend/mcthemax/src/assets/color/color.js
@@ -2,4 +2,5 @@ export const Orange = "#E0633B";
 export const Yellow = "#F3C543";
 export const MenuLightGray = "#EECBC3";
 export const DarkGray = "#949494";
-export const Gray = "a9a9a9";
+export const Gray = "#a9a9a9";
+export const PhantomB = "#191970";
