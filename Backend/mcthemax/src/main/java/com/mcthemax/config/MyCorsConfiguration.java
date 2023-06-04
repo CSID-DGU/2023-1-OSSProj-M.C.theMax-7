0e6ce7d95de2ad5a41c9d0e26b699dfc616bdb26
kwan2 <jgh9987@naver.com>
2023-06-05 05:56:21 +0900

    fix: cors url 변경

--- Backend/mcthemax/src/main/java/com/mcthemax/config/MyCorsConfiguration.java
+++ Backend/mcthemax/src/main/java/com/mcthemax/config/MyCorsConfiguration.java
@@ -14,7 +14,7 @@ public class MyCorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://mcthemax-server-alb-130935858.ap-northeast-2.elb.amazonaws.com")
                         .allowedMethods("GET", "POST")
                         .allowCredentials(true)
                         .maxAge(3600);
