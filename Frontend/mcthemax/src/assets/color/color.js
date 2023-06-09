15cbb953935fabc58e0d33da8f9344bc4496e2b2
g2Min <jimin9320@gmail>
2023-06-10 03:46:10 +0900

    Style: 컬러추가

--- Frontend/mcthemax/src/assets/color/color.js
+++ Frontend/mcthemax/src/assets/color/color.js
@@ -1,2 +1,6 @@
 export const Orange = "#E0633B";
 export const Yellow = "#F3C543";
+export const MenuLightGray = "#EECBC3";
+export const DarkGray = "#949494";
+export const Gray = "#a9a9a9";
+export const PhantomB = "#191970";
