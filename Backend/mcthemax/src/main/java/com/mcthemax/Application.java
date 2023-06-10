41752a610a74aa5c52532ebf9e448a0212464c2c
pgrgrgrgr <parkracoon@naver.com>
2023-06-11 03:50:24 +0900

    feat: upload scoreboard to S3

--- Backend/mcthemax/src/main/java/com/mcthemax/Application.java
+++ Backend/mcthemax/src/main/java/com/mcthemax/Application.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
--- Backend/mcthemax/src/main/java/com/mcthemax/controller/CertpiaController.java
+++ Backend/mcthemax/src/main/java/com/mcthemax/controller/CertpiaController.java
@@ -1,5 +1,7 @@
 package com.mcthemax.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.mcthemax.domain.lecture.CurrentLectureDTO;
 import com.mcthemax.domain.response.MapResult;
 import com.mcthemax.domain.user.Student;
@@ -10,11 +12,18 @@ import com.mcthemax.service.StudentService;
 import com.mcthemax.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.*;
 
 @RestController
@@ -45,5 +54,26 @@ public class CertpiaController {
         resultMap.put("lectures", lectures);
 
         return responseService.getMapResult(resultMap, 200, "ddong");
+    }
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostMapping("certpia/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String fileUrl = "https://" + bucket + "/test" + fileName;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
